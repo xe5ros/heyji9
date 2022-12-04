a7930dc2d5eb40b5f0f417cacb7d5fbf4a45990d
Heyji <53785415+xe5ros@users.noreply.github.com>
2022-12-05 00:09:55 +0900

    Update background.js

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg"];
 function changedBg() {
   const chosenImage = images[Math.floor(Math.random() * images.length)];
   const bgImage = document.createElement("img");
-  bgImage.src = `img/${chosenImage}`;
+  bgImage.src = `${chosenImage}`;
   document.body.style.backgroundImage = `url(${chosenImage})`;
 }
 
